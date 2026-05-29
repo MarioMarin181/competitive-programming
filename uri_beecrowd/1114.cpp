4659d199d2725032586fe876716465d3f524cfb1
Mario Marin <mariomarinmm18@gmail.com>
2026-05-29 16:54:18 -0500

    Add a solution of the next beecrowd problem: 1114

---
+++ uri_beecrowd/1114.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int numero=0;
+	do{
+		cin>>numero;
+		if (numero!=2002){
+			cout<<"Senha Invalida"<<endl;
+		} else{
+			cout<<"Acesso Permitido"<<endl;
+		}
+		
+		
+	}while (numero!=2002);
+	
+	
+	return 0;
+}
