c8621c2d2b64b415d3f70db7fa867b468af3be4e
Mario Marin <mariomarinmm18@gmail.com>
2026-05-28 21:17:27 -0500

    Add a solution of the next beecrowd problem: 1078

---
+++ uri_beecrowd/1078.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n=0;
+	cin>>n;
+	
+	cout<<1<<" x "<<n<<" = "<<(1*n)<<"\n";
+	cout<<2<<" x "<<n<<" = "<<(2*n)<<"\n";
+	cout<<3<<" x "<<n<<" = "<<(3*n)<<"\n";
+	cout<<4<<" x "<<n<<" = "<<(4*n)<<"\n";
+	cout<<5<<" x "<<n<<" = "<<(5*n)<<"\n";
+	cout<<6<<" x "<<n<<" = "<<(6*n)<<"\n";
+	cout<<7<<" x "<<n<<" = "<<(7*n)<<"\n";
+	cout<<8<<" x "<<n<<" = "<<(8*n)<<"\n";
+	cout<<9<<" x "<<n<<" = "<<(9*n)<<"\n";
+	cout<<10<<" x "<<n<<" = "<<(10*n)<<"\n";	
+	
+	
+	
+	return 0;
+}
