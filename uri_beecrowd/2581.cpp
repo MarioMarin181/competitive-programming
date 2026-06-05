ac3befc8832d3a72f4ac4c6d0230a912a90a47da
Mario Marin <mariomarinmm18@gmail.com>
2026-06-04 21:47:58 -0500

    Add a solution of the next beecrowd problem: 2581

---
+++ uri_beecrowd/2581.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n=0;
+	string cadena;
+	cin>>n;
+	
+	while(n--){
+		fflush (stdin);
+		getline(cin,cadena);
+		cout<<"I am Toorg!"<<endl;	
+	}
+	
+	return 0;
+}
