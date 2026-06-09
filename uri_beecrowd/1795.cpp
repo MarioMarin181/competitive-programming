ecaf0e7ed2407a6a3abf7e7b73d0b4c1d873596f
Mario Marin <mariomarinmm18@gmail.com>
2026-06-09 17:04:41 -0500

    Add a solution of the next beecrowd problem: 1795

---
+++ uri_beecrowd/1795.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int R=0;
+	cin>>R;
+
+	cout<<fixed<<setprecision(0)<<pow(3,R)<<"\n";
+
+	return 0;
+}
