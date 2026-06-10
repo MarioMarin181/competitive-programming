f69c669ce1eb0df80edfe8578768b43ecd8d539f
Mario Marin <mariomarinmm18@gmail.com>
2026-06-10 17:32:50 -0500

    Add a solution of the next beecrowd problem: 1143

---
+++ uri_beecrowd/1143.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n=0;
+	
+	cin>>n;
+	
+	for(int i=1; i<=n; i++){
+		cout<<(i)<<" "<<(i*i)<<" "<<(i*i*i)<<endl;
+	}
+	
+	return 0;
+}
